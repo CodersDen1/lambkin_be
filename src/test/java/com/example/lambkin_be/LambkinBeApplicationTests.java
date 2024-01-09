2bb1e64e02956f24a9420fbce1ddea58ea41a942
CodersDen1 <com.github.adbhut@gmail.com>
2024-01-09 19:57:22 +0530

    Initial project commit

---
+++ src/test/java/com/example/lambkin_be/LambkinBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lambkin_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambkinBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
